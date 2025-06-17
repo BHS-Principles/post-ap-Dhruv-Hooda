4c7c9e7fc3ddd0f50caf5049ac68f0ee6f814f43
Dhruv Hooda <Duv3tabest@gmail.com>
2025-06-17 23:09:24 +0000

    yahooo

--- js.js
+++ js.js
@@ -1,5 +1,4 @@
 var TEMP = document.getElementById("temp");
-var TARGET = document.getElementById("target");
 
 var CARD = TEMP.querySelector(".card");
 
@@ -8,68 +7,90 @@ class Game {
     constructor(players, deck) {
         this.players = players;
         this.deck = deck;
+    
+        this.turn = 0;
 
-        this.turn = 0
+        this.drawSurface = document.getElementById("draw");
+        this.finalLabel = document.getElementById("finishLabel");
+        
+        this.deck.shuffle();
 
-        this.play();
-    }
-
-    notOver() {
-        return this.turn++ < 5;
+        this.init();
     }
 
     init() {
         // make this function attach a button to hit or stand & create each player
+        for (var i = 0; i < this.players.length; i++) {
+            this.players[i].init(this);
+            this.players[i].draw(this.drawSurface);
+        }
     }
 
-    play() {
-        // play the game!!
-        // 1; shuffle the cards
-        this.deck.shuffle();
+    end() {
+        // final display
+        console.log("ENDING")
+        
+        // cleanup
+        for (var i = 0; i < this.players.length; i++) {
+            this.players[i].HTML.querySelector("#hit").disabled = true;
+            this.players[i].HTML.querySelector("#stand").disabled = true;
 
-        let win = false;
-
-        let skipped = 0;
-        while (!win) {
-            skipped = 0;
-
-            for (var i = 0; i < this.players.length; i++) {
-                // TODO: implement hit mechanic, right now it automatically just goes up and up
-                var p = this.players[i];
-
-                // check if they have gone over 21
-                var total = p.sum();
-
-                if (total == 21) {
-                    // the player has won, do not give them more cards or similar
-                    skipped += 1;
-                    continue;
-                }
-                
-                // give a card to each players
-                var nextCard = this.deck.pop();
-                console.log(nextCard);
-                console.log(this.players);
-                console.log(p.sum());
-                p.cards.push(nextCard)
-                
-                if ((total + nextCard.value) > 21) {
-                    p.lost = true;
-
-                    this.players.splice(i, 1);
-
-                    console.log(`${p.name} LOST! (value: ${p.sum()})`)
-                }
-                
-                
-            }
-
-            if (skipped == this.players.length) {
-                win = true;
+            if (!this.players[i].lost) {
+                this.finalLabel.textContent = `${this.players[i].name} WON! (value: ${this.players[i].sum()})`;
             }
         }
 
-        console.log(`The players who won are: ${this.players}`);
+        
+    }
+
+    tick() {
+        // check if they have gone over 21
+        this.turn += 1;
+        for (player of this.players) {
+            var total = player.sum();
+
+            if (total == 21) {
+                // the player has won, do not give them more cards or similar
+                player.won = true;
+                this.end();
+                return;
+            }
+
+            if (this.hit) {
+                var nextCard = this.deck.pop();
+                console.log(nextCard);
+                console.log(this.player.sum());
+                player.cards.push(nextCard)
+
+                console.log(nextCard.value);
+                console.log(nextCard.id);
+                console.log("\n\n")
+                
+                
+                player.hit = false;
+            } else {
+                player.stand = false;
+            }
+
+            // draw
+            this.drawSurface.textContent = ''; // clear
+
+            player.draw(this.drawSurface);
+
+            if (newSum > 21) {
+                player.lost = true;
+
+                console.log(`${this.player.name} LOST! (value: ${newSum})`);
+                // the player has lost
+                return;
+            } else if (newSum == 21) {
+                return;
+            }
+
+            if (this.turn > 10) {
+                this.end();
+            }
+        }
     }
 }
 
@@ -78,6 +99,26 @@ class Player {
         this.name = name;
         this.cards = [];
         this.lost = false;
+
+        this.hit = false;
+        this.stand = false;
+
+        this.HTML = TEMP.querySelector("#display").cloneNode(true);
+
+        this.HTML.id = `${name}-display`;
+        this.HTML.querySelector("#title").textContent = name;
+    }
+
+    init(game_ref) {
+        this.HTML.querySelector("#hit").addEventListener("click", () => {
+            this.hit = true;
+            game_ref.tick();
+        });
+
+        this.HTML.querySelector("#stand").addEventListener("click", () => {
+            this.stand = true;
+            game_ref.tick();
+        });
     }
 
     sum() {
@@ -87,6 +128,19 @@ class Player {
         }
 
         return t;
+    }
+
+    draw(appendTo) {
+        var cPos = this.HTML.querySelector("#target");
+        cPos.textContent = ''; // clear
+
+        for (var i = 0; i < this.cards.length; i++) {
+            this.cards[i].draw(cPos);
+        }
+
+        this.HTML.querySelector("#sum").textContent = `Card Total: ${this.sum()}`;
+        
+        appendTo.append(this.HTML);
     }
 }
 
@@ -126,23 +180,23 @@ class Card {
     constructor(value) {
         this.id = value;
         this.suit = Math.floor(value/13);
-        this.value = value % 13;
-
-        this.pic = "card.svg"
-
+        this.value = (value % 13) + 1;
+        this.renderValue = value % 13
+        console.log(value);
+        console.log(this.value);
 
         this.html = CARD.cloneNode(true);
 
-        this.html.innerHTML = "silver: " + this.id;
-
-        this.html.style.backgroundPositionX = -(this.value) + "00%";
+        this.html.style.backgroundPositionX = -(this.renderValue) + "00%";
+        console.log(this.html.style.backgroundPositionX);
+        // TODO: this ONLY renders aces for some reason
         this.html.style.backgroundPositionY = (this.suit * 25) + "%";
         this.html.style.display = "inline-block";
     }
 
     draw(appendTo) {
-        appendTo.append(this.html)
+        appendTo.append(this.html);
     }
 }
 
-new Game([new Player("a")], new Deck(52));
+new Game([new Player("a"), new Player("b"), new Player("c")], new Deck(52));
